24ffd3a8006005cbb00880236f90677d369c5b38
pengyihao <pathology666@163.com>
2022-09-06 10:29:03 +0800

    completely move args check to compile time wrap invalid args to three type of error

--- src/main.zig
+++ src/main.zig
@@ -7,36 +7,118 @@ pub fn Matrix(comptime row: comptime_int, comptime col: comptime_int, comptime T
         const Self = @This();
         const defalutAllocator: std.mem.Allocator = std.heap.page_allocator;
         val: [row]RowType,
-        pub fn init(args: anytype) *Self {
+        const InitArgsCheckResult = enum {
+            StructStruct,
+            StructArray,
+            StructNumberic,
+        };
+        const InitArgsCheckFailedError = error {
+            InvalidFormat,
+            AmountNotMatch,
+            InnerTypeNotSupported,
+        };
+        fn init_args_check(args: anytype) Self.InitArgsCheckFailedError!Self.InitArgsCheckResult {
             const ArgsType = @TypeOf(args);
             const ArgsTypeInfo = @typeInfo(ArgsType);
             if (ArgsTypeInfo != .Struct) {
-                @panic(
-                    \\
-                    \\you should provide a struct like .{*,*...}
-                    \\or a struct like .{.{*,*},.{*,*}...}
-                    \\for init args here
-                );
+                return .InvalidFormat;
             }
-            var self: *Self;
-            switch (ArgsTypeInfo.Struct.fields[0].field_type) {
-                .Struct => {
-                    if(ArgsTypeInfo.Struct.fields.len != row or ArgsTypeInfo.Struct.fields[0].field_type.Struct.fields.len != col) {
-                        @compileError("args ");
+            if (ArgsTypeInfo.Struct.fields.len != col * row) {
+                if(@typeInfo(ArgsTypeInfo.Struct.fields[0].field_type) != .Struct) {
+                    return .AmountNotMatch;
+                }
+                if(ArgsTypeInfo.Struct.fields.len != row) {
+                    return .AmountNotMatch;
+                }
+                inline for (ArgsTypeInfo.Struct.fields) |field| {
+                    switch(@typeInfo(field.field_type)) {
+                        .Struct => {
+                            if(@typeInfo(field.field_type).Struct.fields.len != col) {
+                                return .AmountNotMatch;
+                            }
+                            inline for (@typeInfo(field.field_type).Struct.fields) |field_| {
+                                switch(@typeInfo(field_.field_type)) {
+                                    .Float, .Int, .ComptimeInt, .ComptimeFloat => {
+                                        continue;
+                                    },
+                                    else => {
+                                        return .InvalidFormat;
+                                    }
+                                }
+                            }
+                            return .StructStruct;
+                        },
+                        .Array => {
+                            if(@typeInfo(field.field_type).Array.len != col) {
+                                return .AmountNotMatch;
+                            }
+                            switch (@typeInfo(@typeInfo(field.field_type).Array.child)) {
+                                .Float, .Int, .ComptimeInt, .ComptimeFloat => {
+                                    return .StructStruct;
+                                },
+                                else => {
+                                    return .InnerTypeNotSupported;
+                                }
+                            }
+                            
+                        },
+                        else => {
+                            return .InnerTypeNotSupported;
+                        }
                     }
-                    self = defalutAllocator.create(Self) catch @panic("failed to create memory");
-                },
-                .Float, .Int => {
-                    if(ArgsTypeInfo.Struct.fields.len != row * col) {
-                        @compileError("");
+                }
+            } else {
+                inline for (ArgsTypeInfo.Struct.fields) |field| {
+                    switch(@typeInfo(field.field_type)) {
+                        .Struct, .Array => {
+                            return .AmountNotMatch;
+                        },
+                        .Float, .Int, .ComptimeInt, .ComptimeFloat => {
+                            return .StructNumberic;
+                        },
+                        else => {
+                            return .InnerTypeNotSupported;
+                        }
+                    }
+                }
+            }
+        }
+        pub fn init(args: anytype) *Self {
+            comptime var check_result = Self.init_args_check(args) catch |e| {
+                switch(e) {
+                    InitArgsCheckFailedError.InnerTypeNotSupported => {
+                        @compileError(
+                            \\ value inside tuple is not supported
+                            \\ only supported tuple/struct, array type or numberics
+                            );
+                    },
+                    InitArgsCheckFailedError.InvalidFormat => {
+                        @compileError(
+                            \\ not a valid format for arguments
+                            \\ only supported format like {*, *...} or {{*,...},...}
+                        );
+                    },
+                    InitArgsCheckFailedError.AmountNotMatch => {
+                        @compileError(
+                            \\ not a valid amount for arguments
+                            \\ matrix should have a static size
+                            \\ if you want to auto fill or ignore the extra arguments
+                            \\ try using init_unchecked instead
+                        );
+                    }
+                }
+            };
+            var self: *Self = defalutAllocator.create(Self) catch @panic("failed to create memory");
+            switch (check_result) {
+                .StructNumberic => {
+                    inline for(args) |v, i| {
+                        self.val[i / col][i % col] = v;
                     }
                 },
                 else => {
-                    @compileError("type not supported");
-                },
+                    self.val = args;
+                }
             }
-            
-            self.val = args;
             return self;
         }
         pub fn print(self: *Self) void {
