157aa77b4b6878742ac2ff24184b8f10ca5d8612
pengyihao <pathology666@163.com>
2022-10-09 16:08:17 +0800

    just one time of zig fmt

--- src/utils/iter.zig
+++ src/utils/iter.zig
@@ -3,14 +3,14 @@ const std = @import("std");
 /// check if the iter type is an iterator over type inner
 pub fn isIteratorOf(comptime iter: type, comptime inner: type) bool {
     // iterator should be a struct type
-    if(@typeInfo(iter) != .Struct) return false;
+    if (@typeInfo(iter) != .Struct) return false;
     // should have a next() method
     if (!comptime @hasDecl(iter, "next")) return false;
     const next_fn_info = @typeInfo(@TypeOf(@field(iter, "next")));
     if (next_fn_info != .Fn) return false;
     const return_type = next_fn_info.Fn.return_type;
     // next() should return an optional of type inner
-    if(return_type.? != ?inner) return false;
+    if (return_type.? != ?inner) return false;
     return true;
 }
 /// an empty iterator implementation
@@ -33,19 +33,19 @@ test "iter" {
 /// inner type represents the iterator's inner type
 pub fn isIterable(comptime target: type, comptime inner: type) bool {
     // need to be struct type to have function field
-    if(@typeInfo(target) != .Struct) return false;
-    if(!comptime @hasDecl(target, "iter")) return false;
+    if (@typeInfo(target) != .Struct) return false;
+    if (!comptime @hasDecl(target, "iter")) return false;
     const iter_fn_info = @typeInfo(@TypeOf(@field(target, "iter")));
-    if(iter_fn_info != .Fn) return false;
+    if (iter_fn_info != .Fn) return false;
     const iter_return_type = iter_fn_info.Fn.return_type;
     // iter function should return a iterator that will pass the isIteratorOf trait
-    if(isIteratorOf(iter_return_type.?, ?*const inner)) return false;
-    if(!comptime @hasDecl(target, "iter_mut")) return false;
+    if (isIteratorOf(iter_return_type.?, ?*const inner)) return false;
+    if (!comptime @hasDecl(target, "iter_mut")) return false;
     const iter_mut_fn_info = @typeInfo(@TypeOf(@field(target, "iter_mut")));
-    if(iter_mut_fn_info != .Fn) return false;
+    if (iter_mut_fn_info != .Fn) return false;
     const iter_mut_return_type = iter_mut_fn_info.Fn.return_type;
     // iter function should return a iterator that will pass the isIteratorOf trait
-    if(isIteratorOf(iter_mut_return_type.?, ?*inner)) return false;
+    if (isIteratorOf(iter_mut_return_type.?, ?*inner)) return false;
     return true;
 }
 
